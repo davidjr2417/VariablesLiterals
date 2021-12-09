7b8bbb3dd3e08ea8f2883bb583a4f116dc5db70f
Davidjr2417 <david@hiddengeniusproject.org>
2021-12-09 02:27:16 +0000

    Developed 1.4 Variables & Literals Program

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+ *  This program demonstrates
+ *  how to use variables in a program
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Declare variables to hold data.
+      int    rollno;
+      String name;
+      double marks;
+
+      // Assign values to variables.
+      rollno = 19;
+      name   = "David";
+      marks  = 89.8;
+
+      // Display the message
+      System.out.println("Your roll number is " + rollno);
+      System.out.println("Your name is " + name);
+      System.out.println("Your marks is " + marks);
+   }
+}
